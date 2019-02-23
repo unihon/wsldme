3cd5f6b284bb000105c3e317d315dfbfe6d74db0
unihon <unihon@outlook.com>
2019-02-23 23:42:52 +0800

    wsldme v0.1.0 commit

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # wsldme
-wsldme(WSL Docker-machine Expansion)
+
+wsldme(WSL Docker-machine Expansion) version 0.1.0  
+一个为WSL编写的Docker-machine扩展，目的是让WSL也能快速、方便的使用及管理Docker。
+
+## 要求
+
+### 系统及WSL要求
+
+1. Windows 10 1803及以上（其余版本未经测试）
+2. WSL Debian（其余发行版本未经测试）
+
+### 软件环境及工具
+
+1. [VirtualBox 6.x](https://www.virtualbox.org/wiki/Downloads)
+2. [boot2docker](https://github.com/boot2docker/boot2docker/releases)
+3. [Docker-machine](https://github.com/docker/machine/releases)
+
+#### 说明
+
+VirtualBox下载及安装在Windows主机上；  
+Docker-machine下载Linux版本并安装在WSL上；  
+boot2docker下载后将ISO文件按个人习惯保存在Windows上面适合的目录。
+
+Docker-machine安装好后，确保能执行如下命令：
+
+``` bash
+$ docker-machine -v
+```
+
+## wsldme安装及配置
+
+### 下载
+
+``` bash
+$ wget ... -O /tmp/wsldme
+```
+
+### 配置
+
+``` bash
+$ vim /tmp/wsldme
+```
+
+``` bash
+#* Windows上VBoxManage的路径（VBoxManage是VirtualBox配套的命令行管理工具）
+vbm_path="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+
+#* Windows上boot2docker.iso的路径
+b2d_iso_path="C:\Users\qhlgd\Documents\ISO\boot2docker.iso"
+
+#* VritualBox的虚拟机文件保存路径（可自定义）
+vbf_path="C:\Users\qhlgd\Documents\VirtualBox Files"
+```
+
+wsldme的DHCP配置信息，默认的网段为`192.168.22.00/24`，如VirtualBox现有的DHCP Server与之相同，则会引起冲突，请修改两者其一的网段。
+
+``` bash
+dhcp_ip="192.168.22.22"
+dhcp_netmask="255.255.255.0"
+dhcp_lowerip="192.168.22.100"
+dhcp_upperip="192.168.22.254"
+```
+
+配置好必要项后安装
+
+``` bash
+$ chmod 755 /tmp/wsldme && mv -i /tmp/wsldme /usr/local/bin/
+```
+
+## wsldme使用方法
+
+Usage: wsldme [OPTIONS] COMMAND
+
+**请用wslme命令代替部分docker-machine命令**
+
+| docker-machine | wsldme | 说明 |
+| - | - | - |
+| ~~docker-machine create --driver=virtualbox d-test~~ | wsldme create  d-test | 创建Docker Engine |
+| ~~cocker-machine rm d-test~~ | wsldme rm d-test | 删除Docker Engine |
+| ~~cocker-machine start d-test~~ | wsldme start d-test | 启动Docker Engine |
+| ~~cocker-machine stop d-test~~ | wsldme stop d-test | 停止Docker Engine |
+| ~~cocker-machine restart d-test~~ | wsldme restart d-test | 重启Docker Engine |
---
+++ wsldme.sh
@@ -0,0 +1,400 @@
+#!/usr/bin/env bash
+# wsldme - wsl docker-machine extension
+# < https://github.com/unihon/wsldme >
+# Copyright (c) 2019 Hon Lee
+
+wsldme_version="0.1.0"
+#===============================================
+# configure
+
+#* VBoxManage path
+vbm_path="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+#* boot2docker.iso path
+b2d_iso_path="C:\Users\qhlgd\Documents\ISO\boot2docker.iso"
+#* vritualbox file path
+vbf_path="C:\Users\qhlgd\Documents\VirtualBox Files"
+
+#-----------------------------------------------
+
+# virutal host memory,memory is at least 1023M and cannot be more than half of total memory
+os_memory="1024"
+# vhd format VDI|VMDK|VHD
+vhd_format="VDI"
+# vhd size M,size is at least 1024M.
+vhd_size="8192"
+
+# dhcp options
+dhcp_ip="192.168.22.22"
+dhcp_netmask="255.255.255.0"
+dhcp_lowerip="192.168.22.100"
+dhcp_upperip="192.168.22.254"
+
+# User-friendly settings options
+# user_friendly_eo=true
+wsldme_data_path="$HOME/.wsldme"
+#===============================================
+
+hostname=""
+vh_if=""
+vh_if_field="/VirtualBox/GuestInfo/Net/1/V4/IP"
+
+VBM=$(echo "$vbm_path"|sed -e 's/\\/\//g' -e 's/\ /\\ /g' -e 's/^\(.\):\(.*\)$/\/mnt\/\l\1\2/g')
+shopt -s expand_aliases
+alias VBM=$VBM
+
+#-----------------------------------------------
+
+showHelp(){
+	printf "%s\n" "Usage: wsldme [OPTIONS] COMMAND"
+	printf "%s\n" "WSL Docker-machine Expansion"
+	printf "%s\n" "Version: $wsldme_version"
+	printf "%s\n\n" "Author: Hon Lee - <https://github.com>"
+	printf "%s\n" "Commands:"
+	printf "  %-20s%s\n" "create" "Create a machine"
+	printf "  %-20s%s\n" "rm" "Remove a machine"
+	printf "  %-20s%s\n" "start" "Start a machine"
+	printf "  %-20s%s\n" "stop" "Stop a machine"
+	printf "  %-20s%s\n" "restart" "Restart a machine"
+	printf "  %-20s%s\n" "status" "Get the status of a machine and update the machine IP if the machine's IP changes"
+	printf "  %-20s%s\n" "version" "Show the wsldme version"
+	printf "  %-20s%s\n\n" "help" "Shows a list of commands or help for one command"
+}
+
+loadData(){
+	[ -e "$wsldme_data_path" ] || mkdir "$wsldme_data_path"
+	[ -e "$wsldme_data_path"/wsldme_data ] || cat > "$wsldme_data_path"/wsldme_data << EOF
+# This is wsldme data.
+# If you want to use a new interface,you can clear the data.
+
+wsldme_docker_if=""
+EOF
+
+	# load data from config file.If "wsldme_docker_if" is empty or interface do not exist,create interface
+	vh_if=$(awk -F = '/^wsldme_docker_if="(.*)"/ {gsub(/(")/,"",$2);print $2}' "$wsldme_data_path"/wsldme_data)
+	if [ "$vh_if" == "" ]
+	then
+		createIf
+		[ $? == 1 ] && exit
+		return
+	fi
+
+	if_state=$(VBM list hostonlyifs|awk -F ':' -v IF="$vh_if" '/^Name:/ {gsub(/\r|^[ ]/,"",$2);if($2==IF){print "yes";exit}}')
+	if [ "$if_state" != "yes" ] 
+	then
+		createIf
+		[ $? == 1 ] && exit
+		return
+	fi
+}
+
+dataCheck(){
+	total_memory=$(head -n 1 /proc/meminfo|awk '{print int($2/2^10/2)}')
+	if [ $total_memory -lt $os_memory -o $os_memory -lt 1024 ] 
+	then
+		echo "os memory error."
+		exit
+	elif [ $vhd_size -lt 1024 ]
+	then
+		echo "vhd size error."
+		exit
+	fi
+
+	[ $vhd_format == "VDI" ] && return
+	[ $vhd_format == "VHD" ] && return
+	[ $vhd_format == "VMDK" ] && return
+	echo "vhd format error."
+	exit
+}
+
+createErrorH(){
+	stopHost
+	removeHost
+	exit
+}
+
+checkHost(){
+	if [ "$hostname" == "" ]
+	then
+		echo "Hostname error."
+		exit
+	fi
+
+	vh_path=${vbf_path}\\${hostname}
+	vhd_path=${vh_path}\\${hostname}.${vhd_format,,}
+
+	if [ "$(VBM list vms|egrep ^\"$hostname\")" == "" ]
+	then
+		# Host does not exist
+		return 1
+	else
+		# Host exists
+		return 0
+	fi
+}
+
+checkState(){
+	checkHost 
+	if [ $? -eq 1 ]
+	then
+		echo  "\"$hostname\" does not exist."
+		exit
+	fi
+
+	if [ "$(VBM list runningvms|egrep ^\"$hostname\")" == "" ]
+	then
+		# Host does not run
+		return 1
+	else
+		# Host is running
+		return 0
+	fi
+}
+
+createIf(){
+	echo "Create an interface..."
+
+	vh_if=$(VBM hostonlyif create|sed -r -e "s/Interface\s'(.*?)'\swas\ssuccessfully\screated/\1/g" -e "s/\r//g")
+
+	if [ "$vh_if" == "" ]
+	then
+		echo "Create interface error."
+		return 1
+	fi
+
+	echo "\"$vh_if\" is created."
+
+	if [ "$(VBM list dhcpservers|egrep '^IP:.*?192\.168\.22')" != "" ]
+	then
+		echo "Network segment conflict of the DHCP server, please check."
+		echo "By default wsldme use the \"192.168.22.00/24\" network segment."
+		return 1
+	fi
+
+	VBM hostonlyif ipconfig "$vh_if" --dhcp && VBM dhcpserver add --ifname "$vh_if" --ip $dhcp_ip --netmask $dhcp_netmask --lowerip $dhcp_lowerip --upperip $dhcp_upperip --enable
+	if [ $? -ne 0 ]
+	then
+		echo "DHCP configure error."
+		exit
+	fi
+
+	sed -i "/^wsldme_docker_if/d" "$wsldme_data_path"/wsldme_data
+	echo "wsldme_docker_if=\"$vh_if\"" >> "$wsldme_data_path"/wsldme_data
+	return 0
+}
+
+createHost(){
+	dataCheck
+	checkHost 
+	if [ $? -eq 0 ]
+	then
+		echo  "\"$hostname\" already exists."
+		exit
+	fi
+
+	loadData
+
+	b2d_iso_linux_path_str=$(echo "$b2d_iso_path"|sed -e 's/\\/\//g' -e 's/^\(.\):\(.*\)$/\/mnt\/\l\1\2/g')
+	vh_linux_path_str=$(echo "$vh_path"|sed -e 's/\\/\//g' -e 's/^\(.\):\(.*\)$/\/mnt\/\l\1\2/g')
+
+	mkdir -p "$vh_linux_path_str"
+	cp "$b2d_iso_linux_path_str" "$vh_linux_path_str"
+
+
+	VBM createvm --name $hostname --register || createHost
+	VBM modifyvm $hostname --ostype linux26_64 --memory $os_memory || createHost
+
+	VBM createmedium --filename "$vhd_path"  --format $vhd_format --size $vhd_size || createHost
+
+	VBM storagectl $hostname --name SATA --add sata --hostiocache on || createHost
+
+	VBM storageattach $hostname --storagectl SATA --port 0 --device 0 --type dvddrive --medium "$vh_path/boot2docker.iso" || createHost
+	VBM storageattach $hostname --storagectl SATA --port 1 --device 0 --type hdd --medium "$vhd_path" || createHost
+
+	VBM modifyvm $hostname --nic1 nat --nictype1 82540EM --cableconnected1 on || createHost
+	VBM modifyvm $hostname --nic2 hostonly --nictype2 82540EM --nicpromisc2 deny --cableconnected2 on --hostonlyadapter2 "$vh_if" || createHost
+}
+
+removeHost(){
+	echo "Removing \"$hostname\"..."
+	rm -rf ~/.docker/machine/machines/$hostname
+	VBM unregistervm --delete $hostname || exit
+
+	vh_linux_path_str=$(echo "$vh_path"|sed -e 's/\\/\//g' -e 's/^\(.\):\(.*\)$/\/mnt\/\l\1\2/g')
+	rm -rf "$vh_linux_path_str"
+}
+
+startHost(){
+	checkState
+	if [ $? -eq 0 ]
+	then
+		echo "\"$hostname\" is running. You can restart or stop the machine if you need it."
+		exit
+	fi
+
+	echo "Starting \"$hostname\"..."
+
+	start_state=`VBM startvm $hostname --type headless|egrep -o 'has been successfully started'`
+
+	if [ "$start_state" == "" ]
+	then
+		echo "Start error. You can restart the machine if you need it."
+		exit
+	fi
+
+	echo "\"$hostname\" start successfully!"
+	getIp
+	echo "Started machines may have new IP addresses. You may need to re-run the \`docker-machine env\` command."
+}
+
+stopHost(){
+	checkState
+	if [ $? -eq 1 ]
+	then
+		echo "\"$hostname\" does not run."
+		return
+	fi
+
+	echo "Stoping \"$hostname\"..."
+
+	VBM controlvm $hostname poweroff
+	[ -e ~/.docker/machine/machines/${hostname}/config.json ] && sed -E -i "s/(\"IPAddress\"):\s(\".*?\")/\1: \" \"/" ~/.docker/machine/machines/${hostname}/config.json
+
+	docker-machine env -u
+}
+
+getIp(){
+	echo "Waiting for an IP..."
+
+	for i in $(seq 60)
+	do
+		sleep 2
+		host_ip=$(VBM guestproperty enumerate $hostname --patterns $vh_if_field|egrep -o "([0-9]{1,3}\.){3}[0-9]{1,3}")
+		if [ "$host_ip" != "" ]
+		then
+			break
+		fi
+	done
+
+	if [ "$host_ip" == "" ]
+	then
+		echo "Failed to get IP."
+		[ -e ~/.docker/machine/machines/${hostname}/config.json ] && sed -E -i "s/(\"IPAddress\"):\s(\".*?\")/\1: \" \"/" ~/.docker/machine/machines/${hostname}/config.json
+		exit
+	else
+		[ -e ~/.docker/machine/machines/${hostname}/config.json ] && sed -E -i "s/(\"IPAddress\"):\s(\".*?\")/\1: \"${host_ip}\"/" ~/.docker/machine/machines/${hostname}/config.json
+		echo $host_ip
+	fi
+}
+
+hostState(){
+	checkState
+	if [ $? -eq 0 ]
+	then
+		echo "\"$hostname\" is running."
+		getIp
+	else
+		echo "\"$hostname\" does not run."
+	fi
+}
+
+createKey(){
+	if [ ! -e ~/.ssh/id_rsa ]
+	then
+		echo "Authentication key generation..."
+		ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+		echo -e "\n\n"
+	fi
+}
+
+b2dSh(){
+	mkdir -p /tmp/wsldme/.ssh/
+	\cp -rf ~/.ssh/id_rsa.pub /tmp/wsldme/.ssh/authorized_keys
+
+	echo "Send id_rsa.pub to boot2docker..."
+	echo -e "\033[1;43mNote\033[0m:The password is \"\033[1;32mtcuser\033[0m\"."
+
+	scp -r -o "StrictHostKeyChecking=no" /tmp/wsldme/.ssh docker@${host_ip}:/var/lib/boot2docker
+
+	echo "Make files..."
+	echo -e "\033[1;43mNote\033[0m:The password is \"\033[1;32mtcuser\033[0m\"."
+
+	ssh -Tq docker@${host_ip} << EOF
+sudo cp -f /var/lib/boot2docker/.ssh/authorized_keys /home/docker/.ssh
+sudo tar -cvf /var/lib/boot2docker/userdata.tar /var/lib/boot2docker/.ssh
+sudo rm -rf /var/lib/boot2docker/.ssh
+sudo echo "DOCKER_HOST='-H tcp://0.0.0.0:2376'" > /var/lib/boot2docker/profile
+sudo /etc/init.d/docker restart
+
+for i in \`seq 30\`
+do 
+	[ "\`ss -tnl|awk '{print $4}'|egrep -o '2376'\`" != "" ] && break
+	echo "Waiting..."
+	sleep 1
+done
+exit
+EOF
+
+echo "b2d is processed."
+}
+
+dmCreate(){
+	docker-machine create --driver generic --generic-ssh-user=docker --generic-ip-address=$host_ip --generic-ssh-key ~/.ssh/id_rsa $hostname
+}
+
+initHost(){
+	createHost
+	startHost
+	createKey
+	b2dSh
+	dmCreate
+	cat << EOF
+
++-----------------------------------------------+
+|                                               |
+|      Docker engine created successfully!      |
+|                                               |
++-----------------------------------------------+
+
+EOF
+}
+
+#-----------------------------------------------
+
+hostname=$(echo $2|sed '/\s/d')
+
+case $1 in
+	create)
+		initHost
+		;;
+	rm)
+		echo -n "Are you sure remove\"$hostname\"? (y/n):"
+		read ops
+		[ "$ops" == "n" ] && exit
+		stopHost
+		removeHost
+		;;
+	start)
+		start_time=`date +'%s'`
+		startHost
+		end_time=`date +'%s'`
+		echo "Startup time "$(($end_time-$start_time))s
+		;;
+	stop)
+		stopHost
+		;;
+	restart)
+		stopHost
+		start_time=`date +'%s'`
+		startHost
+		end_time=`date +'%s'`
+		echo "Startup time "$(($end_time-$start_time))s
+		;;
+	status)
+		hostState
+		;;
+	version)
+		echo "$wsldme_version"
+		;;
+	*)
+		showHelp;;
+esac
